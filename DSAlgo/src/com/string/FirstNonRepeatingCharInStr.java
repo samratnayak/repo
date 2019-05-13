3a163779a829ef3878d2da17cd9810910a4a5882
samratnayak <samrat.nayak@outlook.com>
2019-05-13 09:18:20 +0530

    kth highest

--- DSAlgo/src/com/string/FirstNonRepeatingCharInStr.java
+++ DSAlgo/src/com/string/FirstNonRepeatingCharInStr.java
@@ -1,13 +1,17 @@
 package com.string;
 
+import java.util.PriorityQueue;
+
 public class FirstNonRepeatingCharInStr {
 
 	public static void main(String[] args) {
 		
-		String a = "firstinfirstoutnou";
+		String a = "firstinfirstout";
 		
 		String res = findFirstNonRepeating(a);
 		System.out.println(res);
+		String resKth = findKthNonRepeating(a, 2);
+		System.out.println(resKth);
 	}
 
 	static class CountAndIndex implements Comparable<CountAndIndex>{
@@ -54,6 +58,28 @@ public class FirstNonRepeatingCharInStr {
 			}
 		}
 		return String.valueOf((char)charVal);
+	}
+	
+	private static String findKthNonRepeating(String a, int k) {
+		CountAndIndex[] ref = new CountAndIndex[256]; 
+		for(int i = 0; i < a.length() ; i++) {
+			CountAndIndex ob = ref[(int)a.charAt(i)];
+			if(ob != null) {
+				ob.incrementCount();
+			} else {
+				ref[(int)a.charAt(i)] = new CountAndIndex(i);
+			}
+		}
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for(int i = 0; i< ref.length; i++) {
+			if(ref[i] != null && ref[i].count == 1) {
+				pq.add(ref[i].index);
+			}
+		}
+		for(int i = 1; i < k; i++) {
+			pq.poll();
+		}
+		return String.valueOf(a.charAt(pq.poll()));
 	}
 
 }
