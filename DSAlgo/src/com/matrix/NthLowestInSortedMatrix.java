8c0d7a6ef7fea6e771ed122fd43cae4cc6a09127
samratnayak <samrat.nayak@outlook.com>
2019-06-11 21:47:39 +0530

    complete nth highest

--- DSAlgo/src/com/matrix/NthLowestInSortedMatrix.java
+++ DSAlgo/src/com/matrix/NthLowestInSortedMatrix.java
@@ -1,5 +1,7 @@
 package com.matrix;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -11,26 +13,30 @@ public class NthLowestInSortedMatrix {
 		{5, 10, 18, 24},
 		{8, 17, 20, 28},
 		{15, 21, 32, 35}};
-		int a = findNthLowest(m, 5);
+		int a = findNthLowest(m, 8);
 		System.out.println(a);
 	}
 
 	private static int findNthLowest(int[][] m, int n) {
 		Queue<Integer> minHeap = new PriorityQueue<>();
+		Map<Integer, String> indexMap = new HashMap<>();
 		for(int i = 0; i < m[0].length; i++) {
+			indexMap.put(m[0][i], "0-" + i);
 			minHeap.add(m[0][i]);
 		}
 		int i = 0, j = 0, min = -1; 
 		while(n > 0) {
 			min = minHeap.poll();
-			minHeap.add(m[i+1][j]);
-			minHeap.add(m[i][++j]);
-			if(m[i+1][j] < m[i][j+1]) {
-				i++;
-			} else 
-				j++;
+			String[] indexArr = indexMap.get(min).split("-");
+			i = Integer.parseInt(indexArr[0]) + 1;
+			j = Integer.parseInt(indexArr[1]);
+			if(i < m.length) {
+				minHeap.add(m[i][j]);
+				indexMap.put(m[i][j], i+"-"+j);
+			}
+			n--;
 		}
-		return 0;
+		return min;
 	}
 
 }
